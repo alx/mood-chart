35e5823bc502216e23cf57a4cc7156d95311dab2
Alexandre Girard <git@alexgirard.com>
2011-07-04 18:25:53 +0200

    modify capistrano config to deploy on tetalab

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,40 @@
+#$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+#require "rvm/capistrano"                  # Load RVM's capistrano plugin.
+#set :rvm_ruby_string, 'ree@mood_chart'        # Or whatever env you want it to run in.
+
+require 'bundler/capistrano'
+
 set :application, "mood_chart"
-set :repository,  "set your repository location here"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :repository,  "git@github.com:alx/mood-chart.git"
+set :scm, :git
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :ssh_options, { :forward_agent => true, :port => 22104 }
+
+set :user, "alex"
+set :group, "alex"
+set :deploy_to, "/home/alex/#{application}"
+
+role :web, "moods.tetalab.org"                          # Your HTTP server, Apache/etc
+role :app, "moods.tetalab.org"                          # This may be the same as your `Web` server
+role :db,  "moods.tetalab.org", :primary => true # This is where Rails migrations will run
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
+task :link_shared_directories do     
+  run "ln -s #{shared_path}/bot_config.yml #{release_path}/bot/bot_config.yml"
+  run "ln -s #{shared_path}/production.db #{release_path}/production.db"
+end    
+
+after "deploy:update_code", :link_shared_directories
+
